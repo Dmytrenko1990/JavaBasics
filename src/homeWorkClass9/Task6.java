7963e113b6bacc76f1584b03c1a7d8f8c4c9f09e
Dmytrenko1990 <dmitrenkoartem777@gmail.com>
2023-01-17 11:24:41 -0600

    HomeWork Class9

--- src/homeWorkClass9/Task6.java
+++ src/homeWorkClass9/Task6.java
@@ -3,6 +3,9 @@ package homeWorkClass9;
 public class Task6 {
     public static void main(String[] args) {
         String []countries={"Ukraine","Usa","Germany","France","China"};
-        
+        String []capitals={"Kyiv","Washington","Berlin","Paris","Peking"};
+        for(int i=0;i< countries.length;i++){
+            System.out.println("The capital of "+countries[i]+" is "+capitals[i] );
+        }
     }
 }
