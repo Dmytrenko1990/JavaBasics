4e0fef38f75c0affd97e52ede39f16e3d3cf8fd8
Dmytrenko1990 <dmitrenkoartem777@gmail.com>
2023-01-25 17:01:26 -0600

    HomeWorkClass13

---
+++ src/HomeWorkClass13/Task1.java
@@ -0,0 +1,10 @@
+package HomeWorkClass13;
+
+public class Task1 {
+    //Create a String that will hold a sentence.Write a program to get
+    // a new String without any spaces.
+    public static void main(String[] args) {
+        String str="I love java classes.";
+        System.out.println(str.replaceAll(" ",""));
+    }
+}
---
+++ src/HomeWorkClass13/Task5.java
@@ -0,0 +1,14 @@
+package HomeWorkClass13;
+
+public class Task5 {
+    /*How would you check if String is palindrome or not? aba=> true
+    Abbc =>false*/
+    public static void main(String[] args) {
+      String str="acbca";
+        String reverseStr=new StringBuilder(str).reverse().toString();
+        if (str.equals(reverseStr)) {
+            System.out.println(str+" is a palindrome");
+        }
+
+    }
+}
